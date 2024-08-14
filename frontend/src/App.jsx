e0650a73f53a614b60c8e349dc16aace013461b0
Adrian <bontojohnadrian@gmail.com>
2024-08-14 11:48:15 +0800

    created header component and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import "./App.css";
-
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddBlog from "./pages/AddBlog";
 function App() {
   return (
     <>
-      <div>Hello</div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/add-blog" element={<AddBlog />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import GlobalState from "./context/Context.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <GlobalState>
-    <App />
-  </GlobalState>
+  <BrowserRouter>
+    <GlobalState>
+      <App />
+    </GlobalState>
+  </BrowserRouter>
 );
