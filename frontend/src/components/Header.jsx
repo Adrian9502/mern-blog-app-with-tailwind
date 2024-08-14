3b891b1403a274f69655f164b46023fa7a53c9a1
Adrian <bontojohnadrian@gmail.com>
2024-08-14 16:24:52 +0800

    finish designing header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,33 @@
 import { Link } from "react-router-dom";
+import headerlogo from "../../public/headerlogo.png";
+
 export default function Header() {
   return (
-    <div className="bg-emerald-500 flex p-6">
-      <h3 className="flex-1 text-3xl m-0">Mern Blog App</h3>
-      <ul className="list-none flex gap-5">
-        <Link to={"/"}>
-          <li className="text-lg font-bold pointer">Home</li>
-        </Link>
-        <Link to={"/add-blog"}>
-          <li className="text-lg font-bold pointer">Add Blog</li>
-        </Link>
+    <div className="bg-emerald-500 flex justify-between items-center p-3">
+      <Link to="/" className="w-[250px]">
+        <img
+          src={headerlogo}
+          alt="header logo"
+          className="w-full object-cover"
+        />
+      </Link>
+      <ul className="list-none text-base flex flex-col gap-3 text-center lg:flex-row md:flex-row md:text-lg lg:text-lg lg:gap-5">
+        <li>
+          <Link
+            to="/"
+            className="font-bold  rounded-lg px-5 py-1 lg:py-2 md:py-2 hover:bg-slate-900 hover:text-white transition"
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/add-blog"
+            className="font-bold  rounded-lg px-5 py-1 lg:py-2 md:py-2 hover:bg-slate-900 hover:text-white transition"
+          >
+            Add Blog
+          </Link>
+        </li>
       </ul>
     </div>
   );
