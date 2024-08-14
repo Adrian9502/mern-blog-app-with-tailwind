4a1657fa39bb69e29af3aa90f621db96a338774a
Adrian <bontojohnadrian@gmail.com>
2024-08-14 15:51:24 +0800

    installed tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,16 @@ import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AddBlog from "./pages/AddBlog";
+
 function App() {
   return (
-    <>
+    <div className="bg-slate-900 min-h-[100vh]">
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/add-blog" element={<AddBlog />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,14 @@
-import React from "react";
-import classes from "./styles.module.css";
 import { Link } from "react-router-dom";
 export default function Header() {
   return (
-    <div className={classes.header}>
-      <h3>Mern Blog App</h3>
-      <ul>
+    <div className="bg-emerald-500 flex p-6">
+      <h3 className="flex-1 text-3xl m-0">Mern Blog App</h3>
+      <ul className="list-none flex gap-5">
         <Link to={"/"}>
-          <li>Home</li>
+          <li className="text-lg font-bold pointer">Home</li>
         </Link>
         <Link to={"/add-blog"}>
-          <li>Add Blog</li>
+          <li className="text-lg font-bold pointer">Add Blog</li>
         </Link>
       </ul>
     </div>
