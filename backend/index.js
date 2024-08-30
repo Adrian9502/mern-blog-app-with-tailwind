a2ddb55f5f2fc792834035666fbe602891fb24bb
Adrian <bontojohnadrian@gmail.com>
2024-08-30 20:02:27 +0800

    fix origin

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-
-// Initialize express app
-const app = express();
-
-// Import blog router
 const blogRouter = require("./route/blog-route");
-
-// Database connection
 require("./db/db");
+
+const app = express();
 
 // CORS configuration
 const corsOptions = {
   origin:
-    "https://mern-blog-app-with-tailwind-git-main-adrian9502s-projects.vercel.app", // Update this with the correct frontend origin
+    "https://mern-blog-app-with-tailwind-git-main-adrian9502s-projects.vercel.app", // Update with the correct frontend origin
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
 
 // Apply CORS middleware
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Handle preflight requests
 
-// Other middleware
+// Middleware
 app.use(express.json());
-app.options("*", cors(corsOptions));
+
 // Routes
 app.get("/", (req, res) => {
   res.json("Hello");
 });
 
 app.use("/api/blogs", blogRouter);
-app.use("/api", (req, res) => {
-  res.send("Hello");
-});
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
