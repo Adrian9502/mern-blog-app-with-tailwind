8ea5c898986082d2b8493d8a081f02b7044de5f1
Adrian <bontojohnadrian@gmail.com>
2024-08-30 19:52:01 +0800

    reconnect database

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", false);
 
 mongoose
   .connect(
-    "mongodb+srv://bontojohnadrian1:kc73yUKcGf4A@cluster0.g4dramb.mongodb.net/"
+    "mongodb+srv://bontojohnadrian1:kc73yUKcGf4A@cluster0.g4dramb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => console.log("MongoDB Connected"))
   .catch((e) => console.log("Error: ", e.message));
