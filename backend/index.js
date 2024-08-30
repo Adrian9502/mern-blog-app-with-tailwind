c40d9ce2b2e73b96a70df90ec2b1922de65df17a
Adrian <bontojohnadrian@gmail.com>
2024-08-30 19:30:42 +0800

    fix code order

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,24 @@ const express = require("express");
 const cors = require("cors");
 
 // Import blog router
+
+app.options("*", cors());
+app.use(express.json());
+
+const corsOptions = {
+  origin: "https://mern-blog-app-with-tailwind.vercel.app", // Correct origin without the path
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
+app.use(cors(corsOptions));
+
 const blogRouter = require("./route/blog-route");
 
 require("./db/db");
 
 const app = express();
 
-app.options("*", cors());
-app.use(express.json());
-
-const corsOptions = {
-  origin: "https://mern-blog-app-with-tailwind.vercel.app/api/blogs",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-};
-
-app.use(cors(corsOptions));
 app.get("/", (req, res) => {
   res.json("Hello");
 });
