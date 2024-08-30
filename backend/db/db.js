03f77e2a48ceb0abf54e279eef89bf4c3a87cefc
Adrian <bontojohnadrian@gmail.com>
2024-08-30 23:01:40 +0800

    fix connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", false);
 
 mongoose
   .connect(
-    "mongodb+srv://bontojohnadrian1:kc73yUKcGf4A@cluster0.g4dramb.mongodb.net/blogs"
+    "mongodb+srv://bontojohnadrian1:kc73yUKcGf4A@cluster0.g4dramb.mongodb.net/test?retryWrites=true&w=majority"
   )
   .then(() => console.log("MongoDB Connected"))
   .catch((e) => console.log("Error: ", e.message));
