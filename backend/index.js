c5c2d6154800c25f3a23b916eb03bb3c9f921683
Adrian <bontojohnadrian@gmail.com>
2024-08-30 17:59:22 +0800

    hosting in vercel

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,13 @@ require("./db/db");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://mern-blog-app-with-tailwind.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/blogs", blogRouter);
